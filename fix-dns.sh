99011aa8f572cb4e11c4cc8f3cce12a6864906bb
chronp <73487391+chronp@users.noreply.github.com>
2021-03-18 12:00:34 -0400

    Update fix-dns.sh

--- fix-dns.sh
+++ fix-dns.sh
@@ -2,7 +2,10 @@
 ##set namespace
 NAMESPACE="namespace-01"
 kubectl config use-context $NAMESPACE
-##remove old accepted certificates
+
+#copy the root ca to the jumphost
+kubectl cp dell-ca.crt $NAMESPACE/jumpbox:/root/ca.crt
+
 kubectl exec -it jumpbox -- rm -f /root/.ssh/known_hosts
 ##get worker VMs
 VM=$(kubectl get vm)
@@ -15,6 +18,7 @@ do
      #log in to accept new ssh key
      kubectl exec -it jumpbox -- /usr/bin/ssh -o StrictHostKeyChecking=accept-new vmware-system-user@$VMIP 'pwd'
      #add harbor.dell.local to local resolver
-     kubectl exec -it jumpbox  /usr/bin/ssh vmware-system-user@$VMIP "sudo sed -i '$ a 192.168.1.229 harbor.dell.local' /etc/hosts"
+     kubectl exec -it jumpbox /usr/bin/ssh vmware-system-user@$VMIP "sudo sed -i '15s/DNS=/DNS=192.168.1.2/' /etc/systemd/resolved.conf"
+     kubectl exec -it junpbox /usr/bin/ssh vmware-system-user@$VMIP "sudo ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf"
   fi
 done
