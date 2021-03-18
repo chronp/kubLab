2e9d544d4507aaad5bcbf5a031eaeed999149165
chronp <73487391+chronp@users.noreply.github.com>
2021-03-18 10:40:38 -0400

    Create fix-dns.sh

---
+++ fix-dns.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+##set namespace
+NAMESPACE="namespace-01"
+kubectl config use-context $NAMESPACE
+##remove old accepted certificates
+kubectl exec -it jumpbox -- rm -f /root/.ssh/known_hosts
+##get worker VMs
+VM=$(kubectl get vm)
+for VMNAME in $VM
+do
+  #if it is a worker vm
+  if [[ "$VMNAME" == *'tkg'*'worker'* ]]; then
+     #set IP
+     VMIP=$(kubectl -n $NAMESPACE get virtualmachine/$VMNAME -o jsonpath='{.status.vmIp}')
+     #log in to accept new ssh key
+     kubectl exec -it jumpbox -- /usr/bin/ssh -o StrictHostKeyChecking=accept-new vmware-system-user@$VMIP 'pwd'
+     #add harbor.dell.local to local resolver
+     kubectl exec -it jumpbox  /usr/bin/ssh vmware-system-user@$VMIP 'sudo sed -i '$ a 192.168.1.229 harbor.dell.local' /etc/hosts'
+  fi
+done
